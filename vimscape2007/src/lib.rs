0e09925b0a29cf1dbc79727b520ffb61dd63d7d7
Jacob Waldrip <jacob.waldrip@Jacobs-MBP.attlocal.net>
2024-07-14 15:44:23 -0500

    Callable from lua, prints to screen, mac build set up

--- vimscape2007/src/lib.rs
+++ vimscape2007/src/lib.rs
@@ -1,4 +1,13 @@
+use nvim_oxi::{print, Dictionary, Function};
+
 #[nvim_oxi::plugin]
-fn foo() -> i32 {
-	42
+fn vimscape2007() -> nvim_oxi::Result<Dictionary> {
+    let process_batch_fn = Function::from_fn(process_batch);
+    let api = Dictionary::from_iter([("process_batch", process_batch_fn)]);
+    Ok(api)
+}
+
+fn process_batch(_input: String) -> bool {
+    print!("Processing Batch via Rust");
+    true
 }
