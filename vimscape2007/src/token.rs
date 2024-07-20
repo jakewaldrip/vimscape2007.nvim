a0d848cb152a7d9d572fad367828a703e4dd35b7
Jacob Waldrip <jacob.waldrip@Jacobs-MBP.attlocal.net>
2024-07-19 23:49:18 -0500

    Added more tests, a bunch of token cases

--- vimscape2007/src/token.rs
+++ vimscape2007/src/token.rs
@@ -9,10 +9,46 @@ pub enum Token {
     #[regex(r"(?:\d{1,})?[hl]", pull_modifier_from_single_movement)]
     MoveHorizontalBasic(i32),
 
+    #[regex(r"TODO1")]
+    MoveVerticalChunk,
+
     #[regex(r"(?:\d{1,})?[wWeEbB]", pull_modifier_from_single_movement)]
     MoveHorizontalChunk(i32),
-    // #[regex(r"<t_..>")]
-    // UserCommand,
+
+    #[regex(r"[Ff].(?:[;n]{1,})?")]
+    JumpToHorizontal,
+
+    #[regex(r"TODO2")]
+    JumpToVertical,
+
+    #[token("%")]
+    JumpFromContext,
+
+    #[regex(r"TODO3")]
+    CameraMovement,
+
+    #[regex(r"TODO4")]
+    WindowManagement,
+
+    #[regex(r"TODO5")]
+    VisualModeMagic,
+
+    #[regex(r"TODO6")]
+    CommandModeMagic,
+
+    #[regex(r"TODO7")]
+    TextManipulationBasic,
+
+    #[regex(r"TODO8")]
+    TextManipulationAdvanced,
+
+    #[regex(r"(?:[1-9]{1,})?d", pull_modifier_from_single_movement)]
+    #[regex(r"d(?:[1-9]{1,})?[dwWeEbB$^0]", pull_modifier_from_arbitrary_location)]
+    #[regex(r"(?:[1-9]{1,})?x", pull_modifier_from_single_movement)]
+    DeleteText(i32),
+
+    #[token(":w<CR>")]
+    SaveFile,
 }
 
 fn pull_modifier_from_single_movement(lex: &mut Lexer<Token>) -> Option<i32> {
@@ -24,8 +60,13 @@ fn pull_modifier_from_single_movement(lex: &mut Lexer<Token>) -> Option<i32> {
     }
 }
 
+fn pull_modifier_from_arbitrary_location(lex: &mut Lexer<Token>) -> Option<i32> {
+    // TODO
+    Some(1)
+}
+
 #[oxi::test]
-fn lexer_succeeds_basic_vertical_movements() {
+fn basic_vertical_movements() {
     const TEST_INPUT: &str = "10jkk5kj";
     let mut lexer = Token::lexer(TEST_INPUT);
     assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(10))));
@@ -37,25 +78,77 @@ fn lexer_succeeds_basic_vertical_movements() {
 }
 
 #[oxi::test]
-fn lexer_succeeds_basic_horizontal_movements() {
+fn basic_horizontal_movements() {
     const TEST_INPUT: &str = "10hll5lh";
     let mut lexer = Token::lexer(TEST_INPUT);
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(10))));
+    assert_eq!(lexer.slice(), "10h");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(1))));
+    assert_eq!(lexer.slice(), "l");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(1))));
+    assert_eq!(lexer.slice(), "l");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(5))));
+    assert_eq!(lexer.slice(), "5l");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(1))));
+    assert_eq!(lexer.slice(), "h");
     assert_eq!(lexer.next(), None);
 }
 
 #[oxi::test]
-fn lexer_succeeds_chunk_horizontal_movements() {
+fn chunk_horizontal_movements() {
     const TEST_INPUT: &str = "10weEb5b";
     let mut lexer = Token::lexer(TEST_INPUT);
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(10))));
+    assert_eq!(lexer.slice(), "10w");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "e");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "E");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "b");
     assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(5))));
+    assert_eq!(lexer.slice(), "5b");
+    assert_eq!(lexer.next(), None);
+}
+
+#[oxi::test]
+fn mixed_input_movements_hb_hc_vm() {
+    const TEST_INPUT: &str = "jj3jwwbE3wllkk";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(1))));
+    assert_eq!(lexer.slice(), "j");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(1))));
+    assert_eq!(lexer.slice(), "j");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(3))));
+    assert_eq!(lexer.slice(), "3j");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "w");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "w");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "b");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(1))));
+    assert_eq!(lexer.slice(), "E");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(3))));
+    assert_eq!(lexer.slice(), "3w");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(1))));
+    assert_eq!(lexer.slice(), "l");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalBasic(1))));
+    assert_eq!(lexer.slice(), "l");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(1))));
+    assert_eq!(lexer.slice(), "k");
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(1))));
+    assert_eq!(lexer.slice(), "k");
+    assert_eq!(lexer.next(), None);
+}
+
+#[oxi::test]
+fn jump_horizontal_movements() {
+    const TEST_INPUT: &str = "f3;;nfln";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::JumpHorizontal)));
+    assert_eq!(lexer.slice(), "f3;;n");
+    assert_eq!(lexer.next(), Some(Ok(Token::JumpHorizontal)));
+    assert_eq!(lexer.slice(), "fln");
     assert_eq!(lexer.next(), None);
 }
