ae019dd0f6e8f4d3d954213433d988b0f7d8c2dd
Jacob <jakeywaldrip@gmail.com>
2024-08-24 01:37:25 -0500

    Added tests for the rest of the tokens, fixed some, added help page token back

--- vimscape2007/src/token.rs
+++ vimscape2007/src/token.rs
@@ -1,6 +1,5 @@
 use logos::{Lexer, Logos};
 
-// TODO Test callback methods individually? Woudn't hurt
 // TODO figure out macros
 // TODO Consider marks?
 // TODO consider buffers?
@@ -9,6 +8,7 @@ use logos::{Lexer, Logos};
 // TODO figure out save file
 // Potentially this could have something to do with the colon?
 // TODO figure out visual mode
+// TODO for command search, consider reading in n/N keys until esc is pressed
 #[derive(Logos, Debug, PartialEq)]
 pub enum Token {
     #[regex(
@@ -79,7 +79,6 @@ pub enum Token {
     #[token("cawwaw")]
     TextManipulationAdvanced,
 
-    // Needs tests
     // p renders as ""1p
     // yy renders as yyy
     // Y renders as y$
@@ -89,24 +88,22 @@ pub enum Token {
     #[regex(r"(?:[1-9]{1}\d{0,})?y(?:[$w]|iw|aw|<Esc><C-\\><C-N><Esc>)")]
     YankPaste,
 
-    // Needs tests
     #[regex(r"(?:[uU]|<C-R>)")]
     UndoRedo,
 
-    // Needs tests
     // This literally just repeats the keys
     // so i will just let it grant xp for both categories
     #[token(".")]
     DotRepeat,
 
-    // // Needs tests
-    #[regex(r"/.{1,}(?:\|enter\||<Esc>)", was_command_escaped)]
+    // Needs tests
+    #[regex(r"/[A-Za-z0-9]{1,}(?:\|enter\||<Esc>)", was_command_escaped)]
     CommandSearch(bool),
 
-    // Needs tests
     // d$, dw, etc, doubles up on every key, ie dww, d$$, etc
     // If a number is included, it doubles the digits of the number
-    #[regex(r"(?:[1-9]{1}\d{0,})?d", pull_modifier_from_single_movement)]
+    // Why did i write this one? Leave for further investigation
+    // #[regex(r"(?:[1-9]{1}\d{0,})?d", pull_modifier_from_single_movement)]
     #[regex(
         r"d(?:[1-9]{1}\d{0,})?[dwWeEbB$^0][dwWeEbB$^0]",
         pull_modifier_from_arbitrary_location_hacky_version
@@ -114,7 +111,10 @@ pub enum Token {
     #[regex(r"(?:[1-9]{1}\d{0,})?x", pull_modifier_from_single_movement)]
     DeleteText(i32),
 
-    #[token("[a-zA-Z0-9_:<>]", priority = 1)]
+    #[regex(r":(?:h|help) [a-zA-z0-9]{0,}(?:\|enter\||<Esc>)", was_command_escaped)]
+    HelpPage(bool),
+
+    #[regex(r"[a-zA-Z0-9_:<>]", priority = 1)]
     #[token("|enter|", priority = 1)]
     #[token("<Esc>", priority = 1)]
     UnhandledToken,
@@ -450,5 +450,99 @@ fn text_manipulation_advanced_change_arounds() {
     assert_eq!(lexer.next(), Some(Ok(Token::TextManipulationAdvanced)));
     assert_eq!(lexer.next(), Some(Ok(Token::TextManipulationAdvanced)));
     assert_eq!(lexer.next(), Some(Ok(Token::TextManipulationAdvanced)));
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn yank_paste() {
+    const TEST_INPUT: &str = r#"3""3p""1p4""4P3y$y$yiw3yawy<Esc><C-\><C-N><Esc>"#;
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), r#"3""3p"#);
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), r#"""1p"#);
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), r#"4""4P"#);
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), "3y$");
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), "y$");
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), "yiw");
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), "3yaw");
+    assert_eq!(lexer.next(), Some(Ok(Token::YankPaste)));
+    assert_eq!(lexer.slice(), r#"y<Esc><C-\><C-N><Esc>"#);
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn undo_redo() {
+    const TEST_INPUT: &str = "uU<C-R>";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::UndoRedo)));
+    assert_eq!(lexer.slice(), "u");
+    assert_eq!(lexer.next(), Some(Ok(Token::UndoRedo)));
+    assert_eq!(lexer.slice(), "U");
+    assert_eq!(lexer.next(), Some(Ok(Token::UndoRedo)));
+    assert_eq!(lexer.slice(), "<C-R>");
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn dot_repeater() {
+    const TEST_INPUT: &str = "3w.3w";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(3))));
+    assert_eq!(lexer.next(), Some(Ok(Token::DotRepeat)));
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveHorizontalChunk(3))));
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn command_search() {
+    const TEST_INPUT: &str = r#"/testsearch|enter|/testsearch2<Esc>"#;
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::CommandSearch(false))));
+    assert_eq!(lexer.next(), Some(Ok(Token::CommandSearch(true))));
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn delete_text() {
+    const TEST_INPUT: &str = "d33ddddd3xx";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::DeleteText(3))));
+    assert_eq!(lexer.slice(), "d33dd");
+    assert_eq!(lexer.next(), Some(Ok(Token::DeleteText(1))));
+    assert_eq!(lexer.slice(), "ddd");
+    assert_eq!(lexer.next(), Some(Ok(Token::DeleteText(3))));
+    assert_eq!(lexer.slice(), "3x");
+    assert_eq!(lexer.next(), Some(Ok(Token::DeleteText(1))));
+    assert_eq!(lexer.slice(), "x");
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn unhandled_tokens() {
+    const TEST_INPUT: &str = ">|enter|<Esc>";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::UnhandledToken)));
+    assert_eq!(lexer.slice(), ">");
+    assert_eq!(lexer.next(), Some(Ok(Token::UnhandledToken)));
+    assert_eq!(lexer.slice(), "|enter|");
+    assert_eq!(lexer.next(), Some(Ok(Token::UnhandledToken)));
+    assert_eq!(lexer.slice(), "<Esc>");
+    assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn help_page() {
+    const TEST_INPUT: &str = ":h test<Esc>jj:help test|enter|";
+    let mut lexer = Token::lexer(TEST_INPUT);
+    assert_eq!(lexer.next(), Some(Ok(Token::HelpPage(true))));
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(1))));
+    assert_eq!(lexer.next(), Some(Ok(Token::MoveVerticalBasic(1))));
+    assert_eq!(lexer.next(), Some(Ok(Token::HelpPage(false))));
     assert_eq!(lexer.next(), None);
 }
