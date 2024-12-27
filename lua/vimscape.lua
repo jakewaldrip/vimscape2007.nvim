423910687841be56f6420f2fc12c824f1393a013
Jacob <jakeywaldrip@gmail.com>
2024-12-27 03:43:19 -0600

    Putting data from rust into a floating window

--- lua/vimscape.lua
+++ lua/vimscape.lua
@@ -96,9 +96,55 @@ local function dump(o)
 	end
 end
 
+local function round(float)
+	return math.floor(float + 0.5)
+end
+
+Vimscape_stats_bufnr = vim.api.nvim_create_buf(false, true)
+local ui = vim.api.nvim_list_uis()[1]
+local width = round(ui.width)
+local height = round(ui.height)
+
+local footer_text = "[q]uit | [d]etails"
+
+local stat_window_config = {
+	relative = "editor",
+	width = round(width / 2),
+	height = round(height / 2),
+	col = round(width / 4),
+	row = round(height / 4),
+	style = "minimal",
+	focusable = false,
+	border = "double",
+	title = "Vimscape Skills",
+	footer = footer_text,
+}
+
 M.show_data = function()
+	-- Restore modifiable after closing the window
+	vim.api.nvim_create_autocmd({ "BufLeave" }, {
+		callback = function()
+			vim.api.nvim_set_option_value("modifiable", true, {})
+		end,
+	})
+
 	local user_data = vimscape.get_user_data("")
-	print("Data: ", dump(user_data))
+	vim.api.nvim_open_win(Vimscape_stats_bufnr, true, stat_window_config)
+
+	vim.api.nvim_set_option_value("modifiable", true, {})
+	for k, v in pairs(user_data) do
+		print(k, v.skill_name, v.level, v.total_exp)
+		-- Figure out why this isn't triggering
+		-- vim.api.nvim_buf_set_keymap(Vimscape_stats_bufnr, "n", "q", ":lua print('hello')", {})
+		-- Create one for viewing the details as well (another smaller window, do this later its just for vibe)
+		-- vim.api.nvim_buf_set_keymap(Vimscape_stats_bufnr, "n", "d", ":lua print('hello')", {})
+
+		local text = {}
+		local line = "Skill: " .. v.skill_name .. " | Level: " .. v.level .. " | Total Exp: " .. v.total_exp
+		text[1] = line
+		vim.api.nvim_buf_set_lines(Vimscape_stats_bufnr, k, k, false, text)
+	end
+	vim.api.nvim_set_option_value("modifiable", false, {})
 end
 
 vim.on_key(record_keys, ns)
