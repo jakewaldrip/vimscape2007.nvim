88466c0d45d2bff25e4f92eaba3afc1ea5a5e4fa
jacob-waldrip <jacob.waldrip@cityblock.com>
2026-01-04 18:58:30 -0600

    Updated build script to include mac

--- vimscape_backend/build-dev.sh
+++ vimscape_backend/build-dev.sh
@@ -1,1 +1,7 @@
-cargo build && cp target/debug/libvimscape_backend.so ../lua/vimscape_backend.so
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Building for MacOS"
+  cargo build && cp target/debug/libvimscape_backend.dylib ../lua/vimscape_backend.so
+else
+  echo "Building for Linux"
+  cargo build && cp target/debug/libvimscape_backend.so ../lua/vimscape_backend.so
+fi
